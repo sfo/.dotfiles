36b8339999843b09317ef7dcaf6ed0897a8c0cb8
Stanley F <stanley.foerster@gmail.com>
2024-08-20 07:34:28 +0200

    add script to update all settings

---
+++ update_all.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+## update dotfiles
+cd ~/.dotfiles
+git pull
+
+## update OMZ
+cd $ZSH
+git pull
+
+## update OMZ custom stuff
+cd $ZSH_CUSTOM
+git pull
+git submodule update --init --recursive
+
+## update TMUX plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+~/.tmux/plugins/tpm/bin/update_plugins all
+
+## update vim plugins
+vim -c "PluginInstall" -c "qa"
+vim -c "PluginUpdate" -c "qa"
+
